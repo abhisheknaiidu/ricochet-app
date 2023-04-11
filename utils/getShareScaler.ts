7f5235a0841c35b08c4fa39d98bb5f0908f65984
sky cherfa <sherifcherfa@gmail.com>
2023-04-11 06:04:58 -0400

    completed v3 contract integrations

--- utils/getShareScaler.ts
+++ utils/getShareScaler.ts
@@ -1,4 +1,5 @@
 import { readContract } from '@wagmi/core';
+
 import { streamExchangeABI } from 'constants/ABIs/streamExchange';
 import { ExchangeKeys } from 'enumerations/exchangeKeys.enum';
 import { indexIDA } from '../constants/flowConfig';
@@ -10,15 +11,26 @@ export const getShareScaler = async (
 	tokenB: string,
 	chain: number
 ): Promise<number> => {
+	if (
+		exchangeKey === 'twoWayIbUsdIbBTC' ||
+		exchangeKey === 'twoWayIbEthIbUsd' ||
+		exchangeKey === 'twoWayIbBTCIbUsd' ||
+		exchangeKey === 'twoWayIbUsdIbEth'
+		){
+			const { outputIndex } = indexIDA.filter((data) => data.input === tokenA && data.output === tokenB)[0];
+			const outputPool: any = await readContract({
+				address: getExchangeAddressFromKey(exchangeKey) as `0x${string}`,
+				abi: streamExchangeABI,
+				functionName: 'getOutputPool',
+				args: [outputIndex],
+			});
+			const shareScaler = outputPool?.shareScaler! * 1e3;
+			return shareScaler;
+	}
 	//USDC <> IBALLUOUSDC sharescaler is not working, why is that? something to do with the way contracts and config is set up
-	if (chain === 80001 || chain === 10) return 10000;
-	const { outputIndex } = indexIDA.filter((data) => data.input === tokenA && data.output === tokenB)[0];
-	const outputPool: any = await readContract({
-		address: getExchangeAddressFromKey(exchangeKey) as `0x${string}`,
-		abi: streamExchangeABI,
-		functionName: 'getOutputPool',
-		args: [outputIndex],
-	});
-	const shareScaler = outputPool?.shareScaler! * 1e3;
-	return shareScaler;
+	if (chain === 80001 || chain === 10 || chain === 137) {
+		return 10000
+	} else {
+		return 10000;
+	}
 };
